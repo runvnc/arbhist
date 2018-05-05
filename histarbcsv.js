5afbd365e2561ade03f2266687b37331a22d1174
Jason Livesay <runvnc@gmail.com>
2018-05-05 02:57:36 +0000

    mostly done

--- histarbcsv.js
+++ histarbcsv.js
@@ -1,5 +1,5 @@
 const currencylayer = 'cbcc2fe08c78d743101fbe51ad8346bc';
-
+const fs = require('fs');
 const dateformat = require('dateformat');
 const getJson = require('simple-fetch').getJson;
 const AsyncFileReader = require('async-file-reader').AsyncFileReader;
@@ -8,20 +8,29 @@ const vwap = require('vwap');
 const fileExists = require('file-exists');
 
 
-async function readInterval({file, end}) {
+let rates = {};
+
+async function readInterval({file, first, end, rate}) {
   let ret = [];
+  let after = null;
+  rate = 1.0/rate;
+  if (first) ret.push([first[1]*rate,first[0]]);
   let within = false;
   do {
     line = await file.readLine();
     if (line != null) {
       parts = line.split(',')
-      ([time, price, vol] = parts);
+      let [time, price, vol] = parts;
+      time = time *1;
+      price= price *1;
+      vol = vol * 1;
       within = (time <= end);
-      if (within) ret.push([price, vol]);
+      if (within) ret.push([vol,price*rate]); else after=[vol,price];
+      //console.log("inside interval line");
     }
   } while (line != null && within);
   let done = (line == null);
-  return { price: vwap(ret), done };
+  return { price: vwap(ret), done, after };
 }
 
 async function getLineCount(filename) {
@@ -46,7 +55,8 @@ async function requestTimeframeExchange({currency, start, end}) {
   let url = `${base}?access_key=${key}&start_date=${start}`;
   url += `&end_date=${end}&currencies=${currency}`;
   let result = await getJson(url);
-  return result.quotes;
+  console.log(result.quotes);
+  rates[currency] = result.quotes;
 }
 
 async function getTrades({exchange, currency, end}) {
@@ -54,21 +64,46 @@ async function getTrades({exchange, currency, end}) {
   let exists = await fileExists(fname);
   let updated = new Date('01-01-1970');
 
-  if (exists)
-  if (!exists) {
+  if (exists) updated = fs.statSync(fname).mtime;
+
+  if (!exists || updated < end) {
+    console.log('downloading..');	  
     await downloadBTCHist({exchange, currency});
-    
+    await gunzip(fname+'.gz');    
+  } else {
+    console.log('file found');
   }
 }
 
-async function skipToStart({file, start}) {
-  let line = '';
+async function skipToStart({file, start, count}) {
+  let line = '', skipped = 0;
   let time = 0;
   let start_ = start.getTime()/1000;
+ 
   while (line != null && time < start_) {
     line = await file.readLine();
     let parts = line.split(',');
     time = parts[0] * 1;
+    skipped++;
+    let perc =( skipped / count * 100.0).toFixed(0)+'%';   
+    if (skipped % 100000 == 0) console.log('skipped '+skipped+' lines '+perc);
+  }
+
+  console.log("skipped "+skipped+" lines, line time is ", new Date(time*1000));
+}
+
+async function setStatus(params, msg) {
+  console.log(msg);
+}
+
+async function getRate({currency, time}) {
+  let dt = dateformat(time, 'yyyy-mm-dd');
+  //console.log({currency, time, dt, rates, rr:rates[dt]});
+  try {
+    return rates[currency][dt]['USD'+currency];
+  } catch (e) {
+    console.log("getrate error, params = ",{currency,time});
+    return 1;
   }
 }
 
@@ -81,10 +116,11 @@ async function loadAndProcess(params) {
   let fileA = new AsyncFileReader(filenameA);
   let fileB = new AsyncFileReader(filenameB);
 	
-  //let lines = await getLineCount(filenameA);
+  let count = await getLineCount(filenameA);
+  await skipToStart({file: fileA, start, count});
 
-  await skipToStart({file: fileA, start});
-  await skipToStart({file: fileB, start});
+  count = await getLineCount(filenameB);
+  await skipToStart({file: fileB, start, count});
 
   let outfname = exchA+exchAcurr+exchB+exchBcurr+start+'_'+end+'.csv';
   let outcsv = fs.createWriteStream('data/downloads/'+outfname);
@@ -92,22 +128,29 @@ async function loadAndProcess(params) {
   let intervalEnd = start.getTime() / 1000;
   let done = false;
   let total = (end.getTime()/1000) - (start.getTime()/1000);
-
+  let afterA = false; let afterB = false;
   do {
     intervalEnd += intervalSeconds;      
-    let {price:priceA, done:doneA} = await readInterval({file: fileA, end: intervalEnd});
-    let {price:priceB, done:doneB} = await readInterval({file: fileB, end: intervalEnd});
+    let rateA = await getRate({currency:exchAcurr, time: new Date(intervalEnd*1000)});
+    let rateB = await getRate({currency:exchBcurr, time: new Date(intervalEnd*1000)});
+    
+    let {price:priceA, done:doneA, after} = await readInterval({file: fileA, end: intervalEnd, first:afterA, rate:rateA});
+    afterA = after;
+    let priceB, doneB;
+    ({price:priceB, done:doneB, after} = await readInterval({file: fileB, end: intervalEnd, first:afterB, rate:rateB}));
+    afterB = after;
     done = doneA || doneB;
-
-    let dt = dateformat(new Date(intervalEnd*1000), 'yyyy-mm-dd hh:mm:ss a', true);
+    //console.log({priceA, priceB, doneA, doneB, afterA, afterB});
+    
+    let dt = dateformat(new Date(intervalEnd*1000), 'mm-dd-yyyy hh:mm:ss a', true);
     let spread = ((priceA - priceB) / priceA) * 100.0;
     outcsv.write([dt, priceA, priceB, spread.toFixed(2)].join(','));
     outcsv.write('\n');
     let percentDone =((intervalEnd / total) * 100.0).toFixed(0) + '%';
-    if (percentDone % 5 == 0) console.log(percentDone);
+    if (percentDone % 5 == 0) console.log(percentDone); 
   } while (!done);
 
-  outcsv.close();
+  outcsv.close(); 
 }
 
 
@@ -115,19 +158,23 @@ async function histCSV({intervalSeconds, start, end, exchA,exchAcurr,exchB,exchB
   await getTrades({exchange:exchA, currency: exchAcurr, end});
   await getTrades({exchange:exchB, currency: exchBcurr, end});
   
-  let ratesA = await requestTimeframeExchange({currency: exchAcurr, start, end});
-  let ratesB = await requestTimeframeExchange({currency: exchBcurr, start, end});
+  await requestTimeframeExchange({currency: exchAcurr, start, end});
+  await requestTimeframeExchange({currency: exchBcurr, start, end});
 
-  await loadAndProcess({exhA, exchB, exchAcurr, exchBcurr, start, end, intervalSeconds});
+  await loadAndProcess({exchA, exchB, exchAcurr, exchBcurr, start, end, intervalSeconds});
 }
 
 async function test() {
-  await downloadBTCHist({exchange:'bitflyer', currency:'JPY'});
-  console.log(await getLineCount('data/hist/bitflyerJPY.csv'));
+  //await getTrades({exchange:'bitflyer', currency:'JPY', end:new Date('05-01-2018')});
+  //await downloadBTCHist({exchange:'bitflyer', currency:'JPY'});
+  //console.log(await getLineCount('data/hist/bitflyerJPY.csv'));
 
-  let start = new Date('2010-03-01'), end = new Date('2010-04-01'), currency='JPY';
-  let data = await requestTimeframeExchange({start, end, currency});
-  console.log(data);
+  //let start = new Date('2010-03-01'), end = new Date('2010-04-01'), currency='JPY';
+  //let data = await requestTimeframeExchange({start, end, currency});
+  //console.log(data);
+  let start = new Date('03-01-2018'), end = new Date('04-01-2018');
+  await histCSV({intervalSeconds:60,start, end, exchA:'bitflyer',exchB:'bitstamp',exchAcurr:'JPY',
+	         exchBcurr:'USD'});
 }
 
 test().catch(console.error);
